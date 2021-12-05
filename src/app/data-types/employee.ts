351ca50947465bc95e967d478de16eac0bd271b5
Muhammad Nuridin <muhammad.nuridin@skorpoints.com>
2021-12-05 17:35:03 +0700

    handle fetch employee

---
+++ src/app/data-types/employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+  id: number;
+  basicSalary: number;
+  birthDate: string;
+  description: string;
+  email: string;
+  firstName: string;
+  group: string;
+  lastName: string;
+  status: string;
+  username: string;
+}
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import employee from '../_files/employee.json';
-import { NgbModal, ModalDismissReasons, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
 import { ApiService } from './../api.service';
 @Component({
   selector: 'app-employee',
@@ -12,110 +9,16 @@ import { ApiService } from './../api.service';
 })
 export class EmployeeComponent implements OnInit {
   title = 'Employee';
-  formEmployee: FormGroup;
-  submitted = false;
-  isSave = false;
-  closeResult: string;
   dtOptions: DataTables.Settings = {};
-  // employeeList = employee;
-  employeeList = [];
-  employeeDtl: any;
+  employeeList = employee;
 
-  dataGroups = [
-    { value: 1, label: 'Group 1' },
-    { value: 2, label: 'Group 2' },
-    { value: 3, label: 'Group 3' },
-    { value: 4, label: 'Group 4' },
-    { value: 5, label: 'Group 5' },
-    { value: 6, label: 'Group 6' },
-    { value: 7, label: 'Group 7' },
-    { value: 8, label: 'Group 8' },
-    { value: 9, label: 'Group 9' },
-    { value: 10, label: 'Group 10' }
-  ];
+  constructor() { }
 
-  constructor(
-    private modalService: NgbModal,
-    private formBuilder: FormBuilder,
-    private calendar: NgbCalendar,
-    private router: Router,
-    private apiService: ApiService
-  ) { }
-
-  public today = new Date();
-
-  open(content: any, id: any) {
-    if (id) {
-      this.getDetail(id);
-    }
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
-
-  save(): void{
-    this.submitted = true;
-    if (this.formEmployee.invalid) {
-      return;
-    }
-    this.isSave = true;
-    alert('Success save data');
-    location.reload();
-    // console.log('form', this.formEmployee.value);
-  }
-
-  selectToday() {
-    this.formEmployee.value.birthDate = this.calendar.getToday();
-  }
-
-  get formErr() { return this.formEmployee.controls; }
-
-  dataEmployee() {
-    this.formEmployee = this.formBuilder.group({
-      username: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      firtsname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      birthDate: ['', Validators.required],
-      basicSalary: ['', Validators.required],
-      status: ['', Validators.required],
-      group: ['', Validators.required],
-      description: ['', Validators.required],
-    });
-  }
-
-  getData() {
-    this.apiService.getAll().subscribe((data: any) => {
-      this.employeeList = data.result;
-    });
-  }
-
-  getDetail(id: number) {
-    this.apiService.getDetail(id).subscribe((data: any) => {
-      // console.log('detail data', data);
-      this.employeeDtl = JSON.parse(JSON.stringify(data.result));
-    });
-  }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength: 5,
+      pageLength: 10,
       processing: true
     };
-    this.dataEmployee();
-    this.getData();
   }
 }
