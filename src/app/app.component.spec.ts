421a3f6ece39bb053e9be44915a02951c1626144
Muhammad Nuridin <muhammad.nuridin@skorpoints.com>
2021-12-05 17:45:58 +0700

    update app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employee-management'`, () => {
+  it(`should have as title 'angular-employee-management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-management');
+    expect(app.title).toEqual('angular-employee-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('employee-management app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-employee-management app is running!');
   });
 });
